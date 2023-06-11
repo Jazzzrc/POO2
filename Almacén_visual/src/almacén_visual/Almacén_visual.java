19ac3e1f31020143cdad38903e41475971ce78b3
Jazzzrc <102740631+Jazzzrc@users.noreply.github.com>
2023-06-10 23:00:53 -0500

    Add files via upload

---
+++ Almacén_visual/src/almacén_visual/Almacén_visual.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package almacén_visual;
+
+/**
+ *
+ * @author Favio
+ */
+public class Almacén_visual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario formulario = new Formulario();
+        formulario.setVisible(true);
+    }
+}
